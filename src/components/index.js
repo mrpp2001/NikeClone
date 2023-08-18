baece0db79c2896a7efe5659f563d9c669e704cc
mrpp2001 <pratiknpatil2001@gmail.com>
2023-08-19 00:12:37 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Nav from './Nav';
+import Button from './Button'
+import PopularProductCard from './PopularProductCard'
+import ShoeCard from './ShoeCard'
+import ServiceCard from './ServiceCard'
+import ReviewCard from './ReviewCard'
+
+
+
+export {
+    Nav,
+    Button,
+    PopularProductCard,
+    ShoeCard,
+    ServiceCard,
+    ReviewCard
+}
